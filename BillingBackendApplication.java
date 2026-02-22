2cac91dc6e7ddd0cf3bf1faeb31d2205e4ca94c7
shivanijangam-7 <shivanijangam724@gmail.com>
2026-02-22 19:42:54 +0530

    Add files via upload

---
+++ BillingBackendApplication.java
@@ -0,0 +1,9 @@
+package com.example.billing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class BillingBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BillingBackendApplication.class, args);
+    }
+}
